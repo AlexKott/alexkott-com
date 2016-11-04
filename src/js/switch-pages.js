c6799ebf7453a6b1b1a786f088e02426e2e08fce
Alex Kott <tetlokax@gmail.com>
2016-11-04 12:45:34 +0100

    feat(animation): uses css animation to switch between pages

--- src/js/switch-pages.js
+++ src/js/switch-pages.js
@@ -1,31 +1,17 @@
-import scrollWrapper from './scroll-wrapper';
-
 export default function switchPages(onPage, offPage, onPageId) {
     if (onPage.classList.contains('main-list--active')) {
         return;
     }
 
-    const onDir = onPageId === 0 ? 'left' : 'right';
-    const offDir = onPageId === 0 ? 'right' : 'left';
-    const pageWidth = window.innerWidth;
-    let onOffset = pageWidth;
-    let offOffset = 0;
+    const direction = onPageId === 0 ? 'right' : 'left';
 
-    onPage.style[onDir] = `-${onOffset}px`; // eslint-disable-line
+    // multiple adds because IE only uses first argument
     onPage.classList.add('main-list--active');
-
-    function increaseOffset() {
-        if (offOffset <= pageWidth) {
-            offPage.style[offDir] = `-${offOffset}px`; // eslint-disable-line
-            onPage.style[onDir] = `-${onOffset}px`; // eslint-disable-line
-            offOffset += 5;
-            onOffset -= 5;
-            setTimeout(increaseOffset, 2);
-        } else {
-            offPage.classList.remove('main-list--active');
-            onPage.style[onDir] = '0px'; // eslint-disable-line
-        }
-    }
-
-    scrollWrapper().then(() => increaseOffset());
+    onPage.classList.add(`main-list--animate-in-${direction}`);
+    offPage.classList.add(`main-list--animate-out-${direction}`);
+    setTimeout(() => {
+        offPage.classList.remove('main-list--active');
+        offPage.classList.remove(`main-list--animate-out-${direction}`);
+        onPage.classList.remove(`main-list--animate-in-${direction}`);
+    }, 600);
 }
